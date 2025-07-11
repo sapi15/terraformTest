c8e778a9a281983edc5713a71a9e6b4517e9e4bb
dckim15 <dckim3515@gmail.com>
2025-07-11 19:05:38 +0900

    수정

--- seoul/backend.tf
+++ seoul/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-project-tfstate-bucket-2025"
     key            = "seoul/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "my-project-terraform-lock"
     encrypt        = true
   }
 }
